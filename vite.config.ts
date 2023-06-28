20d745095a1a52e14087e8ea5a9854157887e69d
igorsilvaj <igorjsilvabiz@gmail.com>
2023-06-28 12:46:49 -0300

    :wrench:chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { configDefaults, defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     css: false,
-  }
-})
+    exclude: [...configDefaults.exclude, '**/mocks/**'],
+  },
+});
